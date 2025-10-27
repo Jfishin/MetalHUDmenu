b248574970860938ba0e9edacfb7e0511df1a013
Jfishin <joshuagrgurich9287@gmail.com>
2025-10-27 19:53:28 -0400

    Initial Commit

---
+++ MetalHUDmenu/MetalHUDmenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalHUDmenuApp.swift
+//  MetalHUDmenu
+//
+//  Created by Josh Grgurich on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalHUDmenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
